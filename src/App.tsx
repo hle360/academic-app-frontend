33d32c597fce66a7b6248ed69013f3fac8f6c44c
Hanh Le <hle360@gmail.com>
2026-03-29 23:19:13 -0700

    Add routing with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './App.css'
 import StudentList from './components/students/StudentList'
+import Home from './components/Home'
+import { BrowserRouter, Link, Routes, Route } from 'react-router'
 
 function App() {
   return (
-    <> 
-      {/* Add header and footer here */}
-      <div className="container">
-        <StudentList />
-      </div>
-    </>
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav className='navbar'>
+        <Link className='nav-links' to="/">Home</Link> |{" "}
+        <Link className='nav-links' to="/students">Students</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<StudentList />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from "react";
+
+const Home: FC = () => {
+    return (
+        <h3>Welcome to My Academic App</h3>
+    )
+};
+
+export default Home;
